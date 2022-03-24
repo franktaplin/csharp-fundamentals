f4c16d7c6921c3c2758fe71a691a2f7838054e66
Frank Taplin <ftaplin@wsutech.edu>
2022-03-23 21:43:11 -0500

    CAC-103 C# Intro Lab Exercise

--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -19,3 +19,18 @@ void HouseExample()
     House myHouse = new House();
     myHouse.DoorOpenClose();
 }
+
+
+/*
+ This local function is for my first exercise lab.
+ */
+
+void MyLocalFunction()
+{
+    Console.WriteLine("This is my first sentence");
+    Console.WriteLine("I am new to CSharp");
+    Console.WriteLine("This CSharp course is cool");
+    Console.WriteLine("I am learning stuff every day.");
+}
+
+MyLocalFunction();
